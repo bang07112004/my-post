6795533beec04f0ef74d390cef0ec61675363f16
bang07112004 <phong0909191145@gmail.com>
2023-02-16 22:07:40 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import AddPost from "@/components/AddPost";
-import Post from "@/components/Post";
+import AddPost from "../components/AddPost";
+import Post from "../components/Post";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { PostsType } from "./types/Posts";
