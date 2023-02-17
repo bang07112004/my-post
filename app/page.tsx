1c660c6315d3e4a6bb54463604ff02deb1e4ffa4
bang07112004 <phong0909191145@gmail.com>
2023-02-17 20:43:58 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,37 @@
 
 import AllPosts from "../components/AllPosts";
 import AddPost from "../components/AddPost";
+import Post from "../components/Post";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { PostsType } from "../app/types/Posts";
+//Fetch All posts
+const allPosts = async () => {
+  const response = await axios.get("/api/posts/getPosts");
+  return response.data;
+};
 
 export default function Home() {
+  const { data, error, isLoading } = useQuery<PostsType[]>({
+    queryFn: allPosts,
+    queryKey: ["posts"],
+  });
+  if (error) return error;
+  if (isLoading) return "Loading.....";
   return (
     <div>
       <AddPost />
-      <AllPosts />
+      {/* <AllPosts /> */}
+      {data?.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          name={post.user.name}
+          avatar={post.user.image}
+          postTitle={post.title}
+          comments={post.comments}
+        />
+      ))}
     </div>
   );
 }
