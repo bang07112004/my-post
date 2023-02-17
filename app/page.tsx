d387717ec0ec91f28e1d7464a18fb4a849675a0d
bang07112004 <phong0909191145@gmail.com>
2023-02-17 20:06:30 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,28 @@
-"use client";
-
-import Post from "../components/Post";
+// import Post from "../components/Post";
+import AllPosts from "@/components/AllPosts";
 import AddPost from "../components/AddPost";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import { PostsType } from "./types/Posts";
+// import { useQuery } from "@tanstack/react-query";
+// import axios from "axios";
+// import { PostsType } from "./types/Posts";
 
 //Fetch All posts
-const allPosts = async () => {
-  const response = await axios.get("/api/posts/getPosts");
-  return response.data;
-};
+// const allPosts = async () => {
+//   const response = await axios.get("/api/posts/getPosts");
+//   return response.data;
+// };
 
 export default function Home() {
-  const { data, error, isLoading } = useQuery<PostsType[]>({
-    queryFn: allPosts,
-    queryKey: ["posts"],
-  });
-  if (error) return error;
-  if (isLoading) return "Loading.....";
+  // const { data, error, isLoading } = useQuery<PostsType[]>({
+  //   queryFn: allPosts,
+  //   queryKey: ["posts"],
+  // });
+  // if (error) return error;
+  // if (isLoading) return "Loading.....";
 
   return (
     <div>
       <AddPost />
-      {data?.map((post) => (
+      {/* {data?.map((post) => (
         <Post
           key={post.id}
           id={post.id}
@@ -32,7 +31,8 @@ export default function Home() {
           postTitle={post.title}
           comments={post.comments}
         />
-      ))}
+      ))} */}
+      <AllPosts />
     </div>
   );
 }
---
+++ components/AllPosts.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+import Post from "./Post";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { PostsType } from "../app/types/Posts";
+type Props = {};
+
+//Fetch All posts
+const allPosts = async () => {
+  const response = await axios.get("/api/posts/getPosts");
+  return response.data;
+};
+
+function AllPosts({}: Props) {
+  const { data, error, isLoading } = useQuery<PostsType[]>({
+    queryFn: allPosts,
+    queryKey: ["posts"],
+  });
+  if (error) return error;
+  if (isLoading) return "Loading.....";
+  return (
+    <div>
+      {data?.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          name={post.user.name}
+          avatar={post.user.image}
+          postTitle={post.title}
+          comments={post.comments}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default AllPosts;
